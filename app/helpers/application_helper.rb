6a19f2296e9008cfcd69bb3320d810bd3974885e
hotpepper <heyseth@hotmail.com>
2014-10-11 15:38:20 -0400

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
     if page_title.empty?#boolean test
       base_title#implicit return
     else
-      "#{base_title} | #{page_title}"#string interpolation
+	      "#{base_title} | #{page_title}"#string interpolation
     end
   end
 end
